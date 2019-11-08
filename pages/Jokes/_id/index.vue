97124c90c037897f7aa296d51c8116941cdf73a2
Onix <gotnix13@gmail.com>
2019-11-08 00:33:37 -0400

    Added a page to view a selected joke. TODO: Sreach bar 39:38

--- pages/Jokes/_id/index.vue
+++ pages/Jokes/_id/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="">
-      <p>{{$route.params.id}}</p>
+  <div>
+      <nuxt-link to="/jokes">
+          Back to Jokes
+      </nuxt-link>
+      <h2>{{joke}}</h2>
+      <hr/>
+      <small>Joke ID: {{$route.params.id}}</small>
   </div>
 </template>
 
@@ -15,9 +20,20 @@ export default {
             }
         }
     },
-    created(){
-        
-    }
+    async created(){
+        const config = {
+            headers: {
+                "Accept": "application/json"
+            }
+        }
+
+        try {
+            const response = await axios.get(`https://icanhazdadjoke.com/j/${this.$route.params.id}`, config);
+            this.joke = response.data.joke;
+        } catch (error) {
+            console.log(error)
+        }
+    },
 
 }
 </script>
